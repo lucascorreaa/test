b9e5b98b04d1e09392c3b62e97bf564bddc0ae91
Professor Corrêa <lucas.lca14@gmail.com>
2025-05-03 13:44:35 -0300

    feat(app):
    adicionado o projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductList from './components/ProductList'
+
+function App() {
+
+  return (
+   <div>
+    <h1>Catálogo FakeStore</h1>
+    <ProductList />
+   </div>
+  )
+}
+
+export default App
